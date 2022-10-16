fa2b26f1dcd789395b4f9dcbb5e9bbc4fe55ee46
321Rabin <53074799+Rabin321@users.noreply.github.com>
2022-10-16 19:29:29 +0545

    webview

---
+++ lib/widgets/carouselclicktowebview.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class CarouseltowebView extends StatelessWidget {
+  final String? url;
+  // const CarouseltowebView({super.key});
+  const CarouseltowebView({super.key, this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: WebView(
+        initialUrl: url,
+    
+      ),
+    );
+  }
+}
--- lib/widgets/customcarousel.dart
+++ lib/widgets/customcarousel.dart
@@ -1,9 +1,15 @@
 import 'package:bachaau/utils/quotes.dart';
+import 'package:bachaau/widgets/carouselclicktowebview.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CustomCarousel extends StatelessWidget {
   const CustomCarousel({super.key});
+
+  void navigateToRoute(BuildContext context, Widget route) {
+    Navigator.push(context, CupertinoPageRoute(builder: (context) => route));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +18,7 @@ class CustomCarousel extends StatelessWidget {
         options: CarouselOptions(
           aspectRatio: 2.0,
           autoPlay: true,
+          enlargeCenterPage: true,
         ),
         items: List.generate(
           imageSliders.length,
@@ -20,28 +27,61 @@ class CustomCarousel extends StatelessWidget {
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(20),
             ),
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  image: DecorationImage(
-                    image: NetworkImage(
-                      imageSliders[index],
+            child: InkWell(
+              onTap: (){
+                if(index == 0){
+                  navigateToRoute(context,const CarouseltowebView(
+                    url: 'https://nepal.oxfam.org/what-we-do-sustainable-development-programme/women-empowerment-programme',
+                  
+                  ));
+                }
+                else if(index == 1){
+                  navigateToRoute(context,const CarouseltowebView(
+                    url: 'https://nepal.oxfam.org/what-we-do-sustainable-development-programme/women-empowerment-programme',
+                  
+                  ));
+                }
+                 else if(index == 2){
+                  navigateToRoute(context,const CarouseltowebView(
+                    url: 'https://nepal.oxfam.org/what-we-do-sustainable-development-programme/women-empowerment-programme',
+                  
+                  ));
+                }
+                 else {
+                  navigateToRoute(context,const CarouseltowebView(
+                    url: 'https://nepal.oxfam.org/what-we-do-sustainable-development-programme/women-empowerment-programme',
+                  
+                  ));
+                }
+
+
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        imageSliders[index],
+                      ),
+                      fit: BoxFit.cover,
+                    )),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      articleTitle[index],
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        fontSize: MediaQuery.of(context).size.width * 0.04,
+                      ),
                     ),
-                    fit: BoxFit.cover,
-                  )),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  articleTitle[index],
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: Colors.white,
-                    fontSize: MediaQuery.of(context).size.width * 0.04,
                   ),
                 ),
               ),
             ),
-          ),
+          ), 
         ),
       ),
     );
